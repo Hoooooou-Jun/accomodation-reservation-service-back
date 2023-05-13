4cc31fca5eebdb578e07ca2511cc086211a86819
Hoooooou-Jun <jipkim2@gmail.com>
2023-05-13 21:31:19 +0900

    Initial commit

---
+++ src/test/java/com/joey/ars/accomodationreservationservice/AccomodationReservationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joey.ars.accomodationreservationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccomodationReservationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
